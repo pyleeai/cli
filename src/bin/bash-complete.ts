318469a66011583a201dc0d49f66eeb4533b8c98
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2025-05-02 17:43:32 -0400

    biome fix

--- src/bin/bash-complete.ts
+++ src/bin/bash-complete.ts
@@ -3,7 +3,7 @@ import { proposeCompletions } from "@stricli/core";
 import { buildContext } from "../context";
 import { app } from "../app";
 const inputs = process.argv.slice(3);
-if (process.env["COMP_LINE"]?.endsWith(" ")) {
+if (process.env.COMP_LINE?.endsWith(" ")) {
 	inputs.push("");
 }
 await proposeCompletions(app, inputs, buildContext(process));
