21c4d9fe5d9e2e58e767348fc1f426397f6a1337
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2025-05-05 14:08:28 -0400

    fix: Use PORT from environment configuration

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,4 @@
 export const AUTHORITY = process.env.AUTHORITY;
 export const CLIENT_ID = process.env.CLIENT_ID;
 export const REDIRECT_URI = process.env.REDIRECT_URI;
+export const PORT = process.env.PORT;
