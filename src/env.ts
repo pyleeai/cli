8b49830fccd1456b702887dd916bc8e6cfe7d3b0
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2025-05-22 13:19:08 -0400

    feat: Add process import to env module
    Adds the `process` import to the `env` module to access environment
    variables. This change is necessary to support reading the OIDC
    configuration from environment variables.

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,5 @@
+import process from "node:process";
+
 export const PYLEE_OIDC_AUTHORITY = process.env.PYLEE_OIDC_AUTHORITY;
 export const PYLEE_OIDC_CLIENT_ID = process.env.PYLEE_OIDC_CLIENT_ID;
 export const PYLEE_OIDC_REDIRECT_URI = process.env.PYLEE_OIDC_REDIRECT_URI;
