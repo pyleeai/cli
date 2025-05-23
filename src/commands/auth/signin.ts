bf2811e7eab98cf3b724ec7a6396af433954f25a
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2025-05-23 17:36:29 -0400

    feat(auth): Improve sign-in experience
    Adds a check to see if the user is already signed in before attempting to sign in again.
    If the user is already signed in, the command will simply print the user's email and exit
    successfully. If the user is not signed in, the command will attempt to sign in and
    handle the success or failure cases accordingly.

--- src/commands/auth/signin.ts
+++ src/commands/auth/signin.ts
@@ -1,23 +1,28 @@
-import type { LocalContext } from "../../types";
+import type { LocalContext, User } from "../../types";
 import { ExitCode } from "../../types";
 
 export async function signin(this: LocalContext): Promise<void> {
 	const user = await this.user();
 
-	this.process.stdout.write("Signing in...\n");
+	if (user) {
+		this.process.stdout.write(`Signed in as ${user.profile.email}\n`);
+		this.process.exit(ExitCode.SUCCESS);
+	} else {
+		this.process.stdout.write("Signing in...\n");
 
-	try {
-		const user = await this.signIn();
+		try {
+			const user = await this.signIn();
 
-		if (user) {
-			this.process.stdout.write(`Signed in as ${user.profile.email}\n`);
-			this.process.exit(ExitCode.SUCCESS);
-		} else {
-			this.process.stderr.write("Sign in failed!\n");
-			this.process.exit(ExitCode.FAILURE);
+			if (user) {
+				this.process.stdout.write(`Signed in as ${user.profile.email}\n`);
+				this.process.exit(ExitCode.SUCCESS);
+			} else {
+				this.process.stderr.write("Sign in failed!\n");
+				this.process.exit(ExitCode.FAILURE);
+			}
+		} catch {
+			this.process.stderr.write("Sign in errored!\n");
+			this.process.exit(ExitCode.ERROR);
 		}
-	} catch (error) {
-		this.process.stderr.write("Sign in errored!\n");
-		this.process.exit(ExitCode.ERROR);
 	}
 }
