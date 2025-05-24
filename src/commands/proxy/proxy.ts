114e31cc64fed5aeaf5dac10052dd08842d49e82
Jason Morganson <128727+jasonmorganson@users.noreply.github.com>
2025-05-24 16:20:27 -0400

    feat(proxy): Improve user sign-in experience
    Removes unnecessary "Signing in..." message from the console output.
    This change provides a more seamless user experience by removing the
    redundant message, as the sign-in process is already implied by the
    subsequent code execution.

--- src/commands/proxy/proxy.ts
+++ src/commands/proxy/proxy.ts
@@ -9,8 +9,6 @@ export default async function (this: LocalContext): Promise<void> {
 		let user = await this.user();
 
 		if (!user) {
-			this.process.stdout.write("Signing in...\n");
-
 			try {
 				user = await this.signIn();
 
